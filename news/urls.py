758701ad88bffc74b8f7551c5db6c20a63455aed
kingsley ijomah <kingsley.ijomah@gmail.com>
2020-08-08 09:17:45 +0100

    create-update-view

--- news/urls.py
+++ news/urls.py
@@ -1,1 +1,19 @@
-urlpatterns = []
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.ArticleListView.as_view(), name="article-list"),
+    path("article/", views.ArticleCreateView.as_view(), name="article-create"),
+    path("article/<int:pk>", views.ArticleDetailView.as_view(), name="article-detail"),
+    path(
+        "article/<int:pk>/update",
+        views.ArticleUpdateView.as_view(),
+        name="article-update",
+    ),
+    path(
+        "article/<int:pk>/delete",
+        views.ArticleDeleteView.as_view(),
+        name="article-delete",
+    ),
+]
--- news/views.py
+++ news/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Article
+
+
+class ArticleListView(generic.ListView):
+    model = Article
+
+
+class ArticleDetailView(generic.DetailView):
+    pass
+
+
+class ArticleCreateView(generic.CreateView):
+    model = Article
+    fields = ["pub_date", "title", "content"]
+    success_url = "/"
+
+
+class ArticleUpdateView(generic.UpdateView):
+    model = Article
+    fields = ["pub_date", "title", "content"]
+    success_url = "/"
+
+
+class ArticleDeleteView(generic.DeleteView):
+    pass
